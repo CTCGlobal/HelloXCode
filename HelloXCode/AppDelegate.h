b1a93fdfd7a8796a95316a20a01b67b9b78be6e8
fernandokarnagi <fernando.karnagi@ctc-g.com.sg>
2015-10-21 23:26:09 +0800

    New file

---
+++ HelloXCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloXCode
+//
+//  Created by Fernando Karnagi on 21/10/15.
+//  Copyright © 2015 CTC Global. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloXCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloXCode
+//
+//  Created by Fernando Karnagi on 21/10/15.
+//  Copyright © 2015 CTC Global. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
